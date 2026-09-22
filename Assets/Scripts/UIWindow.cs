6643e01c55cf5eb959f60115a80defaa962b8b2a
Guille Oropeza <cdmx2939@amerike.edu.mx>
2026-09-23 00:44:41 +0200

    scripts y ui terminados

--- Assets/Scripts/UIWindow.cs
+++ Assets/Scripts/UIWindow.cs
@@ -1,20 +1,59 @@
+using DG.Tweening;
+using NaughtyAttributes;
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UIWindow : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private RectTransform _canvasRectTransform;
+    [SerializeField] private CanvasGroup _canvasGroup;
+    [SerializeField] private bool _hideOnStart;
+
+    [Header("Animation Settings")]
+    [SerializeField] private float showDuration = 0.5f;
+    [SerializeField] private float hideDuration = 0.5f;
+
+
+    [SerializeField] private Ease showEase= Ease.Linear;
+    [SerializeField] private Ease hideEase = Ease.Linear;
     void Start()
     {
-        
+        Initialize();
+    }
+    public virtual void Initialize()
+    {
+        if (_hideOnStart)
+        {
+            Hide();
+        }
+    }
+    [Button]
+    public virtual void Show(bool instant = false)
+    {
+        if (instant)
+        {
+            _canvasRectTransform.gameObject.SetActive(true);
+        }
+
+        else
+        {
+            RectTransform rectTransform = _canvasGroup.GetComponent<RectTransform>();
+            rectTransform.DOScale(Vector3.one, duration: 0.5f).SetEase(Ease.InBack);
+        }
     }
 
-   public virtual void Show()
+    [Button]
+    public virtual void Hide(bool instant = false)
     {
-
-    }
-
-    public virtual void Hide()
-    {
-
+        if (instant)
+        {
+            _canvasRectTransform.gameObject.SetActive(false);
+        }
+        else
+        {
+            RectTransform rectTransform = _canvasGroup.GetComponent<RectTransform>();
+            rectTransform.DOScale(Vector3.zero, duration: 0.5f).SetEase(Ease.InBack);
+        }
     }
 }
